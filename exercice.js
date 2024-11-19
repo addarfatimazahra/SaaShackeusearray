ada7f8558aff63d2c8d56d99c7acc6582e10fa0b
addarfatimazahra <135432903+addarfatimazahra@users.noreply.github.com>
2024-11-19 14:45:13 +0000

    exo3

--- exercice.js
+++ exercice.js
@@ -1,9 +1,18 @@
 /*exo 1
 let number = [1,2,3,4,5];
-console.log(number)*/
+console.log(number)
 
 //exo2
 let fruits = ["apple", "banana","cherry "];
 console.log (fruits[1]);
+
+//exo3
+let num  =[10, 20,30,40];
+console.log(num);
+num[3]= 50;
+console.log(num);*/
+
+
+
 
 
