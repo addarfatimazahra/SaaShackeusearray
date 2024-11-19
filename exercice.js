1ff8a3055030d43cd06dd69e098ebb7984372f7d
addarfatimazahra <135432903+addarfatimazahra@users.noreply.github.com>
2024-11-19 15:27:24 +0000

    exercice

--- exercice.js
+++ exercice.js
@@ -10,12 +10,25 @@ console.log (fruits[1]);
 let num  =[10, 20,30,40];
 console.log(num);
 num[3]= 50;
-console.log(num);*/
+console.log(num);
 // exo4
 let frt =["mango", "peach", "pear"];
 console.log(frt);
 frt.push("avocado");
 console.log(frt);
+// exo 5
+let animal =["dog", "cat", "bird"]
+console.log(animal);
+delete animal[2];
+console.log(animal);*/
+// exo 6 
+ let user=  {
+    name:"fatimazahra",
+    age:2
+ }
+ console.log({ name: "Alice", age: 30 });
+ 
+
 
 
 
